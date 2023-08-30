ad50dd09ab132e0c2400b9a9f37e404d6ff1bd9a
Hariharan Subramanian <hsubramanian@microsoft.com>
2023-08-30 17:34:50 -0400

    add more UT's

---
+++ src/routingmanager.tests/Configuration/RoutingManagerConfigTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.BridgeToKubernetes.RoutingManager.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.BridgeToKubernetes.RoutingManager.Tests
+{
+    public class RoutingManagerConfigTests
+    {
+        [Fact]
+        public void GetNamespace_ReturnsNamespace()
+        {
+            //setup
+            RoutingManagerConfig routingManagerConfig = new();
+            Environment.SetEnvironmentVariable("NAMESPACE", "testnamespace");
+
+            //act
+            string result = routingManagerConfig.GetNamespace();
+
+            //assert
+            Assert.Equal("testnamespace", result);
+        }
+
+        [Fact]
+        public void GetNamespace_ThrowsExceptionWhenNamespaceIsNull()
+        {
+            //setup
+            RoutingManagerConfig routingManagerConfig = new();
+            Environment.SetEnvironmentVariable("NAMESPACE", "");
+
+            //act
+            var exception = Assert.Throws<ArgumentNullException>(() => routingManagerConfig.GetNamespace());
+
+            //assert
+            Assert.Equal("Value cannot be null. (Parameter '_namespaceName')", exception.Message);
+        }
+    }
+}
---
+++ src/routingmanager.tests/Controllers/PingControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.BridgeToKubernetes.RoutingManager.Controllers;
+using Xunit;
+
+namespace Microsoft.BridgeToKubernetes.RoutingManager.Tests
+{
+    public class PingControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsStatus()
+        {
+           PingController pingController = new();
+           string result = pingController.Get();
+
+            //assert
+            Assert.Equal("Ping!!", result);
+        }
+    }
+}
